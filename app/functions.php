7308f155ce25663d1ed79c5d8a552810e39c3c08
李春波 <adb9@qq.com>
2024-01-20 23:14:03 +0800

    调整env函数位置

--- app/functions.php
+++ app/functions.php
@@ -3,37 +3,6 @@
  * Here is your custom functions.
  */
 
-/**
- * @param string $key
- * @param $default
- * @return array|bool|mixed|string|null
- */
-function env(string $key, $default = null)
-{
-    $value = getenv($key);
-    if ($value === false) {
-        return value($default);
-    }
-    switch (strtolower($value)) {
-        case 'true':
-        case '(true)':
-            return true;
-        case 'false':
-        case '(false)':
-            return false;
-        case 'empty':
-        case '(empty)':
-            return '';
-        case 'null':
-        case '(null)':
-            return null;
-    }
-    if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') {
-        return substr($value, 1, -1);
-    }
-    return $value;
-}
-
 function success($data = [], $code = 0, $msg = '操作成功', $is_array = false)
 {
     if ($is_array) {
---
+++ support/initialization.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @param string $key
+ * @param $default
+ * @return array|bool|mixed|string|null
+ */
+function env(string $key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return value($default);
+    }
+    switch (strtolower($value)) {
+        case 'true':
+        case '(true)':
+            return true;
+        case 'false':
+        case '(false)':
+            return false;
+        case 'empty':
+        case '(empty)':
+            return '';
+        case 'null':
+        case '(null)':
+            return null;
+    }
+    if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') {
+        return substr($value, 1, -1);
+    }
+    return $value;
+}
